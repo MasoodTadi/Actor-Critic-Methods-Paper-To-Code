3cee200b11feeb78f96596452064c46bf3c31755
Masood <m.tadi.ie@gmail.com>
2024-08-09 12:19:19 +0200

    update main_prediction_blackJack

--- Fundamentals/main_prediction_blackJack.py
+++ Fundamentals/main_prediction_blackJack.py
@@ -1,22 +1,24 @@
-import gym
+import gymnasium as gym
 import numpy as np
 from prediction_blackJack import Agent
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    env = gym.make('Blackjack-v0')
+    env = gym.make('Blackjack-v1')
     agent = Agent()
     n_episodes = 500000
     for i in range(n_episodes):
         if i % 50000 == 0:
             print('starting episode', i)
-        observation = env.reset()
+        observation, _ = env.reset()
         done = False
         while not done:
             action = agent.policy(observation)
-            observation_, reward, done, info = env.step(action)
+            observation_, reward, terminated, truncated, info = env.step(action)
             agent.memory.append((observation, reward))
             observation = observation_
+            done = terminated or truncated
         agent.update_V()
     print(agent.V[(21, 3, True)])
     print(agent.V[(4, 1, False)])
+
